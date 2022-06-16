f203c08872accb11bcade7f1862189bd66e8e25e
Leona Widhalm <wi21b101@technikum-wien.at>
2022-06-16 16:45:17 +0200

    add status function

--- src/main/java/com/example/mm/MM.java
+++ src/main/java/com/example/mm/MM.java
@@ -2,6 +2,10 @@ package com.example.mm;
 
 public class MM {
 
+    public String status(){
+        return "Everything is fine";
+    }
+
     public String set(double a){
         if (a >= 99.95){
             return "Everything is fine";
